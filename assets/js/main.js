8dcc09ab77bf45da761ad854807be3ffeba1687b
Valeria Sozzi <valeriasozzi24@gmail.com>
2024-05-06 14:55:13 +0200

    add Fizz Buzz DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+const rowElement = document.querySelector('.row')
+
 for (let i = 1; i <= 100; i++){
-    console.log(i);
+    let colItem;
+
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log('FizzBuzz');
+        colItem = `<div class="col bg_red">FizzBuzz</div>`
+    } else if (i % 3 == 0){
+        console.log('Fizz');
+        colItem = `<div class="col bg_green">Fizz</div>`
+    } else if (i % 5 == 0){
+        console.log('Buzz');
+        colItem = `<div class="col bg_yellow">Buzz</div>`
+    } else {
+        console.log(i);
+        colItem = `<div class="col">${i}</div>`
+    }
+    
+    rowElement.insertAdjacentHTML('beforeend',colItem)
 }
